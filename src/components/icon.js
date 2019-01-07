2e0db64d58f5b27c9b75e695afcef7b6615c2a18
Rogerd Júnior Ribeiro Bitarães <rogerdribeiro@gmail.com>
2019-01-07 21:31:20 -0200

    type of components

--- src/components/icon.js
+++ src/components/icon.js
@@ -5,34 +5,32 @@ const imgStone = require("../imgs/pedra.png");
 const imgPaper = require("../imgs/papel.png");
 const imgScissors = require("../imgs/tesoura.png");
 
-class Icon extends Component {
-    render() {
-        if (this.props.choice === "pedra") {
-            return (
-                <View style={styles.icon}>
-                    <Text style={styles.txtPlayer}>{this.props.player}</Text>
-                    <Image source={imgStone} />
-                </View>
-            );
-        } else if (this.props.choice === "papel") {
-            return (
-                <View style={styles.icon}>
-                    <Text style={styles.txtPlayer}>{this.props.player}</Text>
-                    <Image source={imgPaper} />
-                </View>
-            );
-        } else if (this.props.choice === "tesoura") {
-            return (
-                <View style={styles.icon}>
-                    <Text style={styles.txtPlayer}>{this.props.player}</Text>
-                    <Image source={imgScissors} />
-                </View>
-            );
-        }
-
-        return false;
+const Icon = props => {
+    if (props.choice === "pedra") {
+        return (
+            <View style={styles.icon}>
+                <Text style={styles.txtPlayer}>{props.player}</Text>
+                <Image source={imgStone} />
+            </View>
+        );
+    } else if (props.choice === "papel") {
+        return (
+            <View style={styles.icon}>
+                <Text style={styles.txtPlayer}>{props.player}</Text>
+                <Image source={imgPaper} />
+            </View>
+        );
+    } else if (props.choice === "tesoura") {
+        return (
+            <View style={styles.icon}>
+                <Text style={styles.txtPlayer}>{props.player}</Text>
+                <Image source={imgScissors} />
+            </View>
+        );
     }
-}
+
+    return false;
+};
 
 const styles = StyleSheet.create({
     icon: {
--- src/components/top.js
+++ src/components/top.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { View, Image } from "react-native";
 
 const wallpaper = require("../imgs/jokenpo.png");
 
-class Top extends Component {
-    render() {
-        return (
-            <View>
-                <Image source={wallpaper} />
-            </View>
-        );
-    }
-}
+const Top = () => (
+    <View>
+        <Image source={wallpaper} />
+    </View>
+);
 
 export default Top;
